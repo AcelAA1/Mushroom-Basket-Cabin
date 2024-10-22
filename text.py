49b4814f994838fbc9e697ed82f271729c7e4185
AcelAA1 <alec@andin.se>
2024-10-22 14:21:21 +0200

    MushroomBasketCabin V.2 less extreme illusion of choice. things are functions now. lore in separete text file for redability
    Co-authored-by: Amanden123 <Amanden123@users.noreply.github.com>

---
+++ text.py
@@ -0,0 +1,60 @@
+introduction_text =  """
+You have booked a lovely cabin in the woods.
+Upon arrival you see the door and remember you haven't yet received a key
+You place down your lovely Mushroom basket and inspect the door for clues.
+Upon closer inspection you realise there is no keyhole.
+ """
+enter_cabin_text = """
+You open the door.
+And as you enter the cabin you suddenly realise,
+you forgot to pick up your lovely Mushroom basket. 
+And just as you turn around to get it the door slams shut!
+Oh no! You're trapped as, you try to open the door again you realise it has been locked.               
+"""
+host_responce_1 = """
+Host says 'the door is open there is a code inside'
+Upon reciving this information you decide to open the door
+"""
+host_responce_2 = """
+Host says 'code in house, look around'
+
+"""
+living_room_description = """
+Looking around you realise you are in the living room of the house and in it you see a desk with a computer on it
+and what seems to be the way to the kitchen as well as a master bedroom door.
+"""
+desk_descprition = """
+On the desk you find a note which seems to have a puzzle on it 
+and on the computer you see a post it note that says 'enter password to get door code' 
+"""
+locked_room = """
+The door is looked, 
+So you decide to go back to the living room
+"""
+
+puzzle_1_text = """
+You look at the puzzle and see that it's a note that says, 1+1=?
+
+Now that you have seen the puzzle you decide to enter the solution to the computer.
+To try and login and get the code for the door.
+"""
+
+try_computer_to_early = """
+As you look at the computer you relize you won't know the answer before doing the puzzle.
+So you decide to do the puzzle instead.
+"""
+win_text = """
+You enter the correct password and the code shows up on the screen.
+You make a leep of joy as you sprint to the door and enter the code at the terminal you definetly noticed eariler.
+Your joy can no longer be expressed in words as you pick up your beloved Mushroom Basket as you are finally reunited.
+
+The End.
+
+
+Thanks for playing!
+"""
+
+call_112 = """
+You hear one tone going out and then the connection breaks.
+
+"""
